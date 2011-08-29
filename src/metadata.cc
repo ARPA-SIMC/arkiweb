1c7dc307ded59090361bfe752902b809394ed189
Emanuele Di Giacomo <edigiacomo@arpae.it>
2011-08-29 17:19:17 +0200

    Changed metadata response

--- src/metadata.cc
+++ src/metadata.cc
@@ -1,5 +1,5 @@
 /*
- * extended-summary - web service for extended summary
+ * metadata - web service for metadata
  *
  * Copyright (C) 2011  ARPA-SIM <urpsim@smr.arpa.emr.it>
  *
@@ -20,7 +20,8 @@
  * Author: Emanuele Di Giacomo <edigiacomo@arpa.emr.it>
  */
 #include <iostream>
+#include <arkiweb/cgi.h>
 int main() {
-  std::cout << "HTTP/1.1 500 not implemented" << std::endl << std::endl;
+  std::cout << arkiweb::cgi::HttpStatusHeader(500, "not yet implemented") << std::endl;
   return 0;
 }
