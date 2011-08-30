000054827202376bdc5f89bde94b9efa7ef27ada
Emanuele Di Giacomo <edigiacomo@arpae.it>
2011-08-30 17:32:48 +0200

    Summary web service

--- arkiweb/summary.cc
+++ arkiweb/summary.cc
@@ -22,6 +22,8 @@
 #include <arkiweb/summary.h>
 #include <arki/summary.h>
 #include <arki/summary/stats.h>
+#include <arki/runtime.h>
+#include <arki/formatter.h>
 
 namespace arkiweb {
 namespace summary {
@@ -32,19 +34,103 @@ Printer::Printer(const arki::ConfigFile &cfg,
     : m_cfg(cfg), m_emitter(emitter), 
     m_matcher(arki::Matcher::parse(query)) {}
     
-
 void Printer::print() {
-  m_emitter.start_mapping();
+  arki::Summary summary;
+  for (arki::ConfigFile::const_section_iterator i = m_cfg.sectionBegin();
+       i != m_cfg.sectionEnd(); ++i) {
+    arki::Summary s;
+    arki::ReadonlyDataset *ds = arki::ReadonlyDataset::create(*i->second);
+    ds->querySummary(m_matcher, s);
+    summary.add(s);
+    delete ds;
+  }
+  struct Serialiser : public arki::summary::Visitor {
+    const arki::Summary summary;
+    arki::Emitter &emitter;
+    arki::Formatter *formatter;
 
-  m_emitter.add("fields");
-  m_emitter.start_mapping();
-  m_emitter.end_mapping();
+    Serialiser(const arki::Summary &summary, arki::Emitter &emitter)
+        : summary(summary), emitter(emitter), formatter(arki::Formatter::create()) {}
 
-  m_emitter.add("summarystats");
-  m_emitter.start_mapping();
-  m_emitter.end_mapping();
-  
-  m_emitter.end_mapping();
+    bool operator()(const std::vector< arki::UItem<> >& md,
+                    const arki::UItem<arki::summary::Stats>& stats) {
+      emitter.start_mapping();
+      for (std::vector< arki::UItem<> >::const_iterator i = md.begin();
+           i != md.end(); ++i) {
+        if (!i->defined()) continue;
+        emitter.add((*i)->tag());
+        emitter.start_mapping();
+        if (formatter) emitter.add("desc", (*formatter)(*i));
+        (*i)->serialiseLocal(emitter, formatter);
+        emitter.end_mapping();
+      }
+      emitter.add(stats->tag());
+      emitter.start_mapping();
+      stats->serialiseLocal(emitter, formatter);
+      emitter.end_mapping();
+      emitter.end_mapping();
+    }
+    void serialise() {
+      emitter.start_list();
+      summary.visit(*this);
+      emitter.end_list();
+    } 
+  } serialiser(summary, m_emitter);
+  /*
+  struct Serialiser : public arki::summary::Visitor {
+    const arki::Summary summary;
+    arki::Emitter &emitter;
+
+    std::map<std::string, std::set< arki::UItem<> > > fields;
+    arki::summary::Stats statistics;
+    arki::Formatter *formatter;
+
+    Serialiser(const arki::Summary &summary, arki::Emitter &emitter)
+        : summary(summary), emitter(emitter), formatter(arki::Formatter::create()) {}
+
+    bool operator()(const std::vector< arki::UItem<> >& md,
+                    const arki::UItem<arki::summary::Stats>& stats) {
+      for (std::vector< arki::UItem<> >::const_iterator i = md.begin();
+           i != md.end(); ++i) {
+        if (!i->defined()) continue;
+        fields[(*i)->tag()].insert(*i);
+        statistics.merge(*stats);
+      }
+    }
+
+    void serialise() {
+      summary.visit(*this);
+      emitter.start_mapping();
+
+      emitter.add("fields");
+      emitter.start_mapping();
+      for (std::map<std::string, std::set< arki::UItem<> > >::iterator i = fields.begin();
+           i != fields.end(); ++i) {
+        emitter.add(i->first);
+        emitter.start_list();
+        for (std::set< arki::UItem<> >::const_iterator j = i->second.begin();
+             j != i->second.end(); ++j) {
+          emitter.start_mapping();
+          (*j)->serialiseLocal(emitter, formatter);
+          emitter.add("desc", (*formatter)(*j));
+          emitter.end_mapping();
+        }
+        emitter.end_list();
+      }
+      emitter.end_mapping();
+
+      emitter.add("metadatastats");
+      emitter.start_mapping();
+      statistics.serialiseLocal(emitter, formatter);
+      emitter.end_mapping();
+
+
+      emitter.end_mapping();
+    }
+
+  } serialiser(summary, m_emitter);
+  */
+  serialiser.serialise();
 }
 
 }
