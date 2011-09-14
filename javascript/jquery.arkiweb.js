85afdea966117812c8aace22a397ab97d8af7123
Emanuele Di Giacomo <edigiacomo@arpae.it>
2011-09-14 15:59:24 +0200

    starting version 0.2

---
+++ javascript/jquery.arkiweb.js
@@ -0,0 +1,10 @@
+// Author: Emanuele Di Giacomo <edigiacomo@arpa.emr.it>
+//
+// jQuery plugin for Arkiweb. Requires **Arkiweb.js**.
+//
+(function($) {
+	$.fn.arkiweb = function() {
+		return this.each(function() {
+		});
+	};
+})(jQuery);
