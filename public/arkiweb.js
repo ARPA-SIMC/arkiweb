00a11c32b35e69dc1a7b985d58ab6258b0674e46
Emanuele Di Giacomo <edigiacomo@arpa.emr.it>
2011-09-22 13:32:51 +0200

    restored arkiweb.html

--- public/arkiweb.js
+++ public/arkiweb.js
@@ -11,6 +11,7 @@ var arkiweb = {
 };
 arkiweb.models.Dataset = Backbone.Model.extend({
 	initialize: function(attributes) {
+		this.id = attributes.id;
 		this.name = attributes.name;
 		this.description = attributes.description;
 		this.postprocess = attributes.postprocess;
