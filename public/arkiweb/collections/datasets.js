5e9bb66457041794dfb418a0f339b6173c4d6c69
Emanuele Di Giacomo <edigiacomo@arpae.it>
2013-05-06 23:35:46 +0200

    Changed JSON response for datasets and summary

--- public/arkiweb/collections/datasets.js
+++ public/arkiweb/collections/datasets.js
@@ -1,7 +1,10 @@
 (function() {
 	var Datasets = Backbone.Collection.extend({
 		model: arkiweb.models.Dataset,
-		url: '/datasets'
+		url: '/datasets',
+		parse: function(resp) {
+			return resp.datasets;
+		}
 	});
 
 	this.arkiweb.collections.Datasets = Datasets;
