c33d7c35f0450496b76a439155e15d82d2afd540
Emanuele Di Giacomo <edigiacomo@arpae.it>
2013-07-09 10:53:36 +0200

    json postprocessor

---
+++ public/arkiweb/postprocessors/json.js
@@ -0,0 +1,19 @@
+(function() {
+	var JsonPostproc = arkiweb.views.PostprocessorControl.extend({
+		initialize: function(opts) {
+		},
+		render: function() {
+		},
+		events: {
+		},
+		activate: function() {
+		},
+		deactivate: function() {
+		},
+		getCommand: function() {
+			return "json";
+		}
+	});
+
+	this.arkiweb.postprocessors.seriet = JsonPostproc;
+}());
