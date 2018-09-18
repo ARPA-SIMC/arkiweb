963ac4ba119a85cfb5b239141d55c5db0156d211
Daniele Branchini <dbranchini@arpa.emr.it>
2018-09-18 14:32:03 +0200

    added CI files (fixes #4)

---
+++ .travis-build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -exo pipefail
+
+image=$1
+
+if [[ $image =~ ^centos: ]]
+then
+    pkgcmd="yum"
+    builddep="yum-builddep"
+    sed -i '/^tsflags=/d' /etc/yum.conf
+    yum install -q -y epel-release
+    yum install -q -y @buildsys-build
+    yum install -q -y yum-utils
+    yum install -q -y yum-plugin-copr
+    yum install -q -y git
+    yum copr enable -q -y simc/stable epel-7
+elif [[ $image =~ ^fedora: ]]
+then
+    pkgcmd="dnf"
+    builddep="dnf builddep"
+    sed -i '/^tsflags=/d' /etc/dnf/dnf.conf
+    dnf install -q -y @buildsys-build
+    dnf install -q -y 'dnf-command(builddep)'
+    dnf install -q -y git
+    dnf copr enable -q -y simc/stable
+fi
+
+$builddep -y fedora/SPECS/arkiweb.spec
+
+if [[ $image =~ ^fedora: || $image =~ ^centos: ]]
+then
+    pkgname=arkiweb-master
+    mkdir -p ~/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
+    cp fedora/SPECS/arkiweb.spec ~/rpmbuild/SPECS/arkiweb.spec
+    cp fedora/SOURCES/* ~/rpmbuild/SOURCES/
+    git archive --prefix=$pkgname/ --format=tar HEAD | gzip -c > ~/rpmbuild/SOURCES/$pkgname.tar.gz
+    rpmbuild -ba --define "srcarchivename $pkgname" ~/rpmbuild/SPECS/arkiweb.spec
+    find ~/rpmbuild/{RPMS,SRPMS}/ -name "*rpm" -exec cp -v {} . \;
+    # TODO upload ${pkgname}*.rpm to github release on deploy stage
+else
+    autoreconf -ifv
+    ./configure
+    make check TEST_VERBOSE=1
+fi
