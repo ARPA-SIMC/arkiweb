849eb8ff7e2b4f7762bd66feec04e81585423663
Enrico Zini <enrico@enricozini.org>
2025-02-24 18:32:51 +0100

    Test behaviour of /datasets depending on users

--- arkiweb/arkimet/tests/test_datasets.py
+++ arkiweb/arkimet/tests/test_datasets.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 
+from arkiweb.arkimet.models import User
 from arkiweb.arkimet.views import DatasetsView
 
 from .utils import APITestMixin
@@ -9,7 +10,7 @@ from .utils import APITestMixin
 class DatasetsTests(APITestMixin[DatasetsView], TestCase):
     view_class = DatasetsView
 
-    def test_get(self) -> None:
+    def test_get_anonymous(self) -> None:
         self.add_dataset("test1")
         self.add_dataset("test2", postprocess=["foo", "bar"])
         response = self.client.get(reverse("arkimet:datasets"))
@@ -30,6 +31,37 @@ class DatasetsTests(APITestMixin[DatasetsView], TestCase):
                         "id": "test2",
                         "name": "test2",
                         "allowed": "false",
+                        "bounding": "",
+                        "description": "",
+                        "postprocess": ["foo", "bar"],
+                    },
+                ]
+            },
+        )
+
+    def test_get_restricted(self) -> None:
+        user = User.objects.create(username="user", arkimet_restrict="mygroup")
+        self.client.force_login(user)
+        self.add_dataset("test1")
+        self.add_dataset("test2", postprocess=["foo", "bar"], restrict=["mygroup"])
+        response = self.client.get(reverse("arkimet:datasets"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.json(),
+            {
+                "datasets": [
+                    {
+                        "id": "test1",
+                        "name": "test1",
+                        "allowed": "false",
+                        "bounding": "",
+                        "description": "",
+                        "postprocess": [""],
+                    },
+                    {
+                        "id": "test2",
+                        "name": "test2",
+                        "allowed": "true",
                         "bounding": "",
                         "description": "",
                         "postprocess": ["foo", "bar"],
