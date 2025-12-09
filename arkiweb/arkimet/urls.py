f692ff6b4764372a7123556bd8c0131841a7ed0d
Emanuele Di Giacomo <edigiacomo@arpae.it>
2025-12-09 13:12:46 +0100

    Removed trailing slash from API URLs

--- arkiweb/arkimet/urls.py
+++ arkiweb/arkimet/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = "arkimet"
 
 urlpatterns = [
-    path("data/", views.DataView.as_view(), name="data"),
-    path("datasets/", views.DatasetsView.as_view(), name="datasets"),
-    path("fields/", views.FieldsView.as_view(), name="fields"),
-    path("summary/", views.SummaryView.as_view(), name="summary"),
+    path("data", views.DataView.as_view(), name="data"),
+    path("datasets", views.DatasetsView.as_view(), name="datasets"),
+    path("fields", views.FieldsView.as_view(), name="fields"),
+    path("summary", views.SummaryView.as_view(), name="summary"),
 ]
